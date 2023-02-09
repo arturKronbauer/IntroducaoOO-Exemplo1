061df0d35f02ea172fb4a728216a6ddfeb010f23
Artur Henrique Kronbauer <arturhk@gmail.com>
2023-02-09 07:50:22 -0300

    Criando a Classe Triângulo

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,23 +2,27 @@ package aplicacao;
 
 import java.util.Scanner;
 
+import entidades.Triangulo;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		Triangulo x, y;
+		x = new Triangulo();
+		y = new Triangulo();
 		System.out.println("Informe a dimensão dos três lados do triângulo X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Informe a dimensão dos três lados do triângulo Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		System.out.printf("Área do triângulo X = %.4f%n", areaX);
 		System.out.printf("Área do triângulo Y = %.4f%n", areaY);
 		if (areaX > areaY) {
