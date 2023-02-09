324d9eaab11879f03cde99a0db628ac5555c18e8
Artur Henrique Kronbauer <arturhk@gmail.com>
2023-02-09 08:45:32 -0300

    Criando o método area

--- src/entidades/Triangulo.java
+++ src/entidades/Triangulo.java
@@ -2,4 +2,9 @@ package entidades;
 
 public class Triangulo {
 	public double a, b, c;
+
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
